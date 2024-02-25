c268094729b991b5c320d325e3c8daa2af35c993
root <Akua-95>
2024-02-25 22:18:01 +0000

    API

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -25,7 +25,7 @@ def get_amenity(amenity_id):
     amenity = storage.get(Amenity, amenity_id)
     if amenity is None:
         abort(404)
-    return jsonify(amenity.to_dict())
+     return jsonify(amenity.to_dict())
 
 
 @app_views.route('/amenities/<string:amenity_id>', methods=['DELETE'],
